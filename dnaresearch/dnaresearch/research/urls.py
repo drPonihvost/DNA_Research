ebd3e48de1c9d5235f347d827d08f4cfea62e59e
drPonihvost <kudrovpn@gmail.com>
2022-04-01 12:24:51 +0700

    Таблица для объектов research

--- dnaresearch/dnaresearch/research/urls.py
+++ dnaresearch/dnaresearch/research/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('research/all', Researches.as_view(), name='register'),
-    path('research/<int:pk>', Research.as_view(), name='research_info'),
+    path('research/<int:research_id>', Research.as_view(), name='research_detail'),
     path('research/<int:research_id>/person', persons, name='persons'),
     path('research/<int:research_id>/person/<int:person_id>', Person.as_view(), name='person'),
     path('research/person/all', Persons.as_view(), name='all_persons'),
