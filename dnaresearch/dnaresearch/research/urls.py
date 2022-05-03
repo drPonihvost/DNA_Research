710e084527c77e79126ed62ba42cc049fe8e3982
drPonihvost <kudrovpn@gmail.com>
2022-05-03 22:31:16 +0700

    Docker-compose для dev и prod

--- dnaresearch/dnaresearch/research/urls.py
+++ dnaresearch/dnaresearch/research/urls.py
@@ -1,19 +1,19 @@
 from django.urls import path, re_path
 
-from .views import *
+from . import views
 
 urlpatterns = [
-    path('research/', Researches.as_view(), name='register'),
-    path('research/add', ResearchForm.as_view(), name='research_form'),
-    path('research/export/', single_export, name='research_export'),
-    path('research/<int:research_id>', Research.as_view(), name='research_detail'),
-    path('research/<int:research_id>/edit', ResearchUpdateForm.as_view(), name='research_update_form'),
-    path('research/<int:research_id>/remove', ResearchDeleteForm.as_view(), name='research_delete'),
-    path('research/<int:research_id>/register', ResearchRegister.as_view(), name='research_register'),
-    path('research/person', AllPersons.as_view(), name='all_persons'),
-    path('research/<int:research_id>/person', Persons.as_view(), name='persons'),
-    path('research/<int:research_id>/person/<int:person_id>', Person.as_view(), name='person_detail'),
-    path('research/<int:research_id>/person/add', PersonForm.as_view(), name='person_form'),
-    path('research/<int:research_id>/person/<int:person_id>/update', PersonUpdate.as_view(), name='person_update'),
-    path('research/<int:research_id>/person/<int:person_id>/delete', PersonDelete.as_view(), name='person_delete'),
+    path('research/', views.Researches.as_view(), name='register'),
+    path('research/add', views.ResearchForm.as_view(), name='research_form'),
+    path('research/export/', views.single_export, name='research_export'),
+    path('research/<int:research_id>', views.Research.as_view(), name='research_detail'),
+    path('research/<int:research_id>/edit', views.ResearchUpdateForm.as_view(), name='research_update_form'),
+    path('research/<int:research_id>/remove', views.ResearchDeleteForm.as_view(), name='research_delete'),
+    path('research/<int:research_id>/register', views.ResearchRegister.as_view(), name='research_register'),
+    path('research/person', views.AllPersons.as_view(), name='all_persons'),
+    path('research/<int:research_id>/person', views.Persons.as_view(), name='persons'),
+    path('research/<int:research_id>/person/<int:person_id>', views.Person.as_view(), name='person_detail'),
+    path('research/<int:research_id>/person/add', views.PersonForm.as_view(), name='person_form'),
+    path('research/<int:research_id>/person/<int:person_id>/update', views.PersonUpdate.as_view(), name='person_update'),
+    path('research/<int:research_id>/person/<int:person_id>/delete', views.PersonDelete.as_view(), name='person_delete'),
 ]
--- dnaresearch/docs/README.md
+++ dnaresearch/docs/README.md
@@ -15,9 +15,38 @@
 Журнал проверяемых лиц (person):
 
 Разверстка:
-cd .\dnaresearch\dnaresearch\
-python manage.py makemigrations
-python manage.py migrate
-python manage.py loaddata criminal_articles.json
-python manage.py loaddata research_data.json
-python manage.py runserver
+- cd .\dnaresearch\dnaresearch\
+- python manage.py makemigrations
+- python manage.py migrate
+- python manage.py loaddata criminal_articles.json
+- python manage.py loaddata research_data.json
+- python manage.py runserver
+
+env.dev
+- DEBUG=1
+- SECRET_KEY=foo
+- DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]
+- SQL_ENGINE=django.db.backends.postgresql
+- SQL_DATABASE=dnaresearch_dev
+- SQL_USER=dnaresearch
+- SQL_PASSWORD=dnaresearch
+- SQL_HOST=db
+- SQL_PORT=5432
+- DATABASE=postgres
+
+env.prod
+- DEBUG=1
+- SECRET_KEY=change_me
+- DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]
+- SQL_ENGINE=django.db.backends.postgresql
+- SQL_DATABASE=dnaresearch_prod
+- SQL_USER=dnaresearch
+- SQL_PASSWORD=dnaresearch
+- SQL_HOST=db
+- SQL_PORT=5432
+- DATABASE=postgres
+
+env.prod.db
+- POSTGRES_USER=dnaresearch
+- POSTGRES_PASSWORD=dnaresearch
+- POSTGRES_DB=dnaresearch_prod
